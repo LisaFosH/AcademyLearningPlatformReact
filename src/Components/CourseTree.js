2f10c68fb51e10095237fb512d72a324586ee90b
Ingvild Unstad <inging18@hotmail.com>
2021-01-15 11:33:11 +0100

    Course tree and mock courses are now working, using map.

--- src/Components/CourseTree.js
+++ src/Components/CourseTree.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import TreeView from "@material-ui/lab/TreeView";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import TreeItem from "@material-ui/lab/TreeItem";
 import "./CourseTree.css";
 import api from "../api";
-
-// const useStyles = makeStyles({
-//   root: {
-//     //  If we wanna make changes to the looks of the tree (which we do, eventually)
-//   },
-// });
 
 class CourseTree extends React.Component {
   constructor(props) {
@@ -26,6 +19,7 @@ class CourseTree extends React.Component {
   componentDidMount() {
     api.fetchCourses().then((result) => {
       this.setState({ courses: result });
+      console.log(result);
     });
   }
 
@@ -36,37 +30,28 @@ class CourseTree extends React.Component {
       courses = result;
     });
 
-    // const classes = useStyles();
-
     return (
       <div className="course-tree">
-        <Typography variant="h3">Courses</Typography>{" "}
+        <Typography variant="h3">Courses</Typography>
         <TreeView
-          // className={classes.root}
           defaultCollapseIcon={<ExpandMoreIcon />}
           defaultExpandIcon={<ChevronRightIcon />}
         >
-          {this.state.courses.map((emp) => (
-            <TreeItem nodeId={emp.id} key={emp.id} label={emp.title}>
-              <TreeItem label="Week 1" />
-              <TreeItem label="Week 2" />
-              <TreeItem label="Week 3" />
+          {this.state.courses.map((course) => (
+            <TreeItem nodeId={course.id} key={course.id} label={course.title}>
+              {course.modules.map((module) => (
+                <TreeItem
+                  nodeId={module.id}
+                  key={module.id}
+                  label={module.title}
+                >
+                  {module.pages.map((page) => (
+                    <TreeItem label={page.title} />
+                  ))}
+                </TreeItem>
+              ))}
             </TreeItem>
           ))}
-          {/* The below TreeItems are for testing purposes */}
-          <TreeItem nodeId="56" label="C#Summer2020">
-            <TreeItem nodeId="57" label="Pre-studies">
-              <TreeItem label="Week 1" />
-              <TreeItem label="Week 2" />
-              <TreeItem label="Week 3" />
-              <TreeItem label="Week 4" />
-              <TreeItem label="Week 5" />
-              <TreeItem label="Week 6" />
-            </TreeItem>
-            <TreeItem nodeId="30" label="Java" />
-            <TreeItem nodeId="40" label="HTML & CSS" />
-            <TreeItem nodeId="50" label="JavaScript" />
-          </TreeItem>
         </TreeView>
       </div>
     );
--- src/api.js
+++ src/api.js
@@ -2,10 +2,102 @@ const apiurl = process.env.REACT_APP_API_URL;
 
 // Fetching the courses
 var fetchCourses = function (component) {
-  return fetch(`${apiurl}api/apicourses`, {
-    credentials: "include",
-    mode: "cors",
-  }).then((res) => res.json());
+  var mockCourses = [
+    {
+      id: 1,
+      title: "It works!",
+      description: "I hope :D",
+      enrollments: null,
+      modules: [
+        {
+          id: 10,
+          title: "This is my module",
+          pages: [
+            {
+              id: 1,
+              title: "Page 1 on it works",
+              learningMaterial: "This is my page on it works!",
+            },
+            {
+              id: 2,
+              title: "Page 2",
+              learningMaterial: "This is my second page!",
+            },
+            {
+              id: 3,
+              title: "Page 3",
+              learningMaterial: "This is my third page!",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "This is my course",
+      description: "My course is amazing!",
+      enrollments: null,
+      modules: [
+        {
+          id: 20,
+          title: "This is my amazing module",
+          pages: [
+            {
+              id: 1,
+              title: "Page 1",
+              learningMaterial: "This is my amazing page!",
+            },
+            {
+              id: 2,
+              title: "Page 2",
+              learningMaterial: "This is my second page!",
+            },
+            {
+              id: 3,
+              title: "Page 3",
+              learningMaterial: "This is my third page!",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "All about plants",
+      description: "Really, do we have to? Yes!",
+      enrollments: null,
+      modules: [
+        {
+          id: 30,
+          title: "This is my plant module",
+          pages: [
+            {
+              id: 1,
+              title: "Page 1",
+              learningMaterial: "This is my plant page!",
+            },
+            {
+              id: 2,
+              title: "Page 2",
+              learningMaterial: "This is my second page!",
+            },
+            {
+              id: 3,
+              title: "Page 3",
+              learningMaterial: "This is my third page!",
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  // return fetch(`${apiurl}api/apicourses`, {
+  //   credentials: "include",
+  //   mode: "cors",
+  // }).then((res) => res.json());
+
+  return Promise.resolve(mockCourses);
 };
 
 // Fetching the username
